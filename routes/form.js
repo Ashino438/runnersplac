150a66c8a69752c39ab88fd42c97037053e378f5
Ashino438 <zoom.ashino2@gmail.com>
2025-05-26 10:17:18 +0900

    modify

--- routes/form.js
+++ routes/form.js
@@ -36,19 +36,6 @@ router.get('/form', async (req, res) => {
 });
 
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBbFaRwIiYw61X5yKXhvt1nGw-MRSgagLo",
-  authDomain: "mystlide.firebaseapp.com",
-  databaseURL: "https://mystlide-default-rtdb.firebaseio.com",
-  projectId: "mystlide",
-  storageBucket: "mystlide.firebasestorage.app",
-  messagingSenderId: "823773802251",
-  appId: "1:823773802251:web:823d531f428f02f463fc0e",
-  measurementId: "G-E2V49FQSMX"
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
 
 router.get('/', (req, res) => {
   res.render('form');
